dc31929085b79cad09925f3e6400d4ac6677628f
minddoi <minddoi493@gmail.com>
2025-08-17 00:19:15 +0900

    feat: main 검색창 추가

---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import styled from 'styled-components';
+import SearchIcon from '../assets/icons/main_search.png';
+
+const SearchBar = () => {
+  return (
+    <SearchContainer>
+        <Input placeholder="Heukseok Market" />
+        <img src={SearchIcon} alt="검색" />
+    </SearchContainer>
+  )
+}
+
+export default SearchBar
+
+const SearchContainer = styled.div`
+  display: flex;
+  width: 335px;
+  padding: 10px 16px;
+  justify-content: space-between;
+  align-items: center;
+  border-radius: 8px;
+  background: #EAEAEA;
+  margin: 30px auto 0;
+`;
+
+
+const Input = styled.input`
+  flex: 1;
+  border: none;
+  background: transparent;
+  font-size: 14px;
+  outline: none;
+`;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
 import React, { useRef } from 'react'
 import { Link } from 'react-router-dom';
 import Layout from '../components/Layout';
+import ImageSlider from '../components/ImageSlider';
+import styled from 'styled-components';
+import SearchBar from '../components/SearchBar';
 
 const Home = () => {
 
   return (
     <>
         <Layout>
+            <OverlapHeader>
+                <ImageSlider />
+            </OverlapHeader>
+
+            <SearchBar />
         </Layout>
         {/* <Link to="/receipt">영수증 페이지로 이동</Link> */}
     </>
@@ -14,3 +22,7 @@ const Home = () => {
 }
 
 export default Home
+
+const OverlapHeader = styled.div`
+  margin-top: calc(-1 * (var(--header-h) + 8px));
+`;
