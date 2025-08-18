f6bb0c01161745a5fe4f603ec75f5e4d2f05a091
minddoi <minddoi493@gmail.com>
2025-08-18 13:00:39 +0900

    style: Header와 Layout 패딩 및 레이아웃 수정

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,34 +2,19 @@ import React, { useRef } from 'react'
 import Header from './Header'
 import Footer from './Footer'
 import styled from 'styled-components';
+import TabBar from './TabBar';
 
-const Layout = ({children}) => {
-    const contentRef = useRef(null);
+const Layout = ({ children, overlapHeader = false }) => {
 
   return (
     <>
       <Header />
       <Outer>
         <Inner>
-          <AppContent ref={contentRef}>{children}</AppContent>
+          <AppContent $overlap={overlapHeader}>{children}</AppContent>
         </Inner>
       </Outer>
-      <Footer />
-      
-      {/* <Header />
-      <div className="outer">
-        <div className="inner">
-          <main className="app-content" ref={contentRef}>
-                {/* <section className="section">
-                <h1>테스트데스트</h1>
-                {[...Array(200)].map((_, i) => (
-                    <p key={i}>테스트테스트 {i + 1}</p>
-                ))}
-                </section>
-            </main>
-        </div>
-      </div>
-      <Footer /> */}
+      <TabBar />
     </>
   )
 }
@@ -43,13 +28,16 @@ const Outer = styled.div`
 
 const Inner = styled.div`
   margin: 0 auto;
-  width: 420px; 
+  width: 375px; 
   background: #fff;
   box-shadow: 0 10px 30px rgba(0,0,0,0.15);
 
   min-height: 100dvh;  /* 브라우저 지원 시 */
+  overflow: hidden;
 `;
 
 const AppContent = styled.main`
-  padding: calc(var(--header-h) + 8px) 16px 16px;
+  padding-top: ${p => (p.$overlap ? '0' : 'calc(var(--header-h) + 8px)')};
+  padding-bottom: 64px;      /* 탭바에 안 가리게 */
+  overflow-x: hidden;
 `;
